50148c7a6b6e7ca2471b23b4917cd6f4193ebccf
Ali2k3 <92156046+Ali2k3@users.noreply.github.com>
2024-09-19 13:18:38 +0530

    Password reset done

--- first_project/settings.py
+++ first_project/settings.py
@@ -128,3 +128,5 @@ MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, "uploads")
 AUTH_USER_MODEL="authentication.Author"
 LOGIN_REDIRECT_URL="/news/"
+EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
+
